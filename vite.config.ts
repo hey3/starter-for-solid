ac6693e348dab5879b429a497ad08b3f13d8791b
hey3 <oh1kohei@gmail.com>
2022-03-31 04:38:10 +0900

    add globals for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import solidPlugin from 'vite-plugin-solid'
 export default defineConfig({
   plugins: [solidPlugin()],
   test: {
+    globals: true,
     environment: 'jsdom',
     transformMode: {
       web: [/\.[jt]sx?$/],
