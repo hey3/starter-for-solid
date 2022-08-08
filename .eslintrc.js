94705dd237e029f07f37dd67593254e6b051483e
hey3 <oh1kohei@gmail.com>
2022-08-08 23:33:26 +0900

    refactor: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,39 +7,50 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   ignorePatterns: ['node_modules/*', '!.storybook'],
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  plugins: ['unused-imports'],
+  rules: {
+    'import/export': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'index', 'object', 'type'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx'],
+        paths: ['node_modules/', 'node_modules/@types'],
+      },
+    },
+  },
   overrides: [
     {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
+      files: ['**/*.jsx'],
+      extends: ['plugin:solid/recommended'],
+    },
+    {
+      files: ['**/*.tsx'],
+      extends: ['plugin:solid/typescript'],
+    },
+    {
+      files: ['**/*.[jt]sx'],
       env: {
         browser: true,
       },
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:solid/typescript',
-        'plugin:jsx-a11y/recommended',
-        'plugin:tailwindcss/recommended',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'eslint-config-prettier',
-      ],
-      plugins: ['unused-imports'],
+      extends: ['plugin:jsx-a11y/recommended', 'plugin:tailwindcss/recommended'],
+    },
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
       rules: {
-        'import/export': 'off',
-        'import/order': [
-          'error',
-          {
-            groups: ['builtin', 'external', 'internal', 'parent', 'index', 'object', 'type'],
-            alphabetize: { order: 'asc', caseInsensitive: true },
-          },
-        ],
-        'unused-imports/no-unused-imports': 'error',
         '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/explicit-function-return-type': [
           'warn',
@@ -50,13 +61,9 @@ module.exports = {
         ],
       },
     },
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts', '.tsx'],
-        paths: ['node_modules/', 'node_modules/@types'],
-      },
+    {
+      files: ['*'],
+      extends: ['prettier'],
     },
-  },
+  ],
 }
