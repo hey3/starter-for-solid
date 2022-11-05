2fd0e02a7d4b9e4888a21480742f91487867693b
hey3 <oh1kohei@gmail.com>
2022-11-05 23:04:35 +0900

    feat: update storybook config for css modules

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
 import type { DecoratorFunction } from '@storybook/addons'
 import { render } from 'solid-js/web'
+import { ThemeProvider } from '../src/stores/ThemeStore'
 import '../src/styles/reset.css'
 import '../src/styles/global.css'
 
@@ -7,17 +8,23 @@ let disposeStory: () => void
 
 export const decorators: DecoratorFunction<Element>[] = [
   Story => {
+    // Reset theme for each story.
+    window.localStorage.setItem('theme', 'light')
+
     if (disposeStory) {
       disposeStory()
     }
-    const root = document.getElementById('root')
-    const solid = document.createElement('div')
+    const root = window.document.getElementById('root')
+    const solid = window.document.createElement('div')
 
     if (!root) throw new Error('Failed to find the root element')
 
     solid.setAttribute('id', 'solid-root')
     root.appendChild(solid)
-    disposeStory = render(Story, solid)
+    // JSX type using solid-js
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    disposeStory = render(() => <ThemeProvider>{Story()}</ThemeProvider>, solid)
     return solid
   },
 ]
