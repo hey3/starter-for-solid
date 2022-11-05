fa464a0bad091bf1f9ac9f19cf81e08d3d0f8175
hey3 <oh1kohei@gmail.com>
2022-11-05 19:18:49 +0900

    feat: global and reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { render } from 'solid-js/web'
 import { App } from './app'
+import './styles/reset.css'
+import './styles/global.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
