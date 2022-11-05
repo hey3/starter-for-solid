4180fe7bca68d927d5de72b849d3149cf0123207
hey3 <oh1kohei@gmail.com>
2022-11-05 23:03:12 +0900

    feat: theme provider system with css modules

---
+++ src/stores/ThemeStore.tsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, createRenderEffect, createEffect } from 'solid-js'
+import type { ParentComponent } from 'solid-js'
+import { createStore } from 'solid-js/store'
+
+type Theme = 'light' | 'dark'
+
+type ThemeContextState = {
+  activeTheme: Theme
+}
+
+type ThemeContext = [
+  state: ThemeContextState,
+  actions: {
+    toggleTheme: () => void
+  }
+]
+
+const ThemeContext = createContext<ThemeContext | null>(null)
+
+export const ThemeProvider: ParentComponent = props => {
+  createRenderEffect(() => {
+    let theme: Theme
+    const savedTheme = window.localStorage.getItem('theme') as Theme
+    if (savedTheme) {
+      theme = savedTheme
+    } else {
+      theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+    window.document.documentElement.setAttribute('data-theme', theme)
+  })
+
+  const [state, setState] = createStore<ThemeContextState>({
+    activeTheme: window.document.documentElement.getAttribute('data-theme') as Theme,
+  })
+
+  createEffect(() => {
+    if (state.activeTheme) {
+      window.document.documentElement.setAttribute('data-theme', state.activeTheme)
+      window.localStorage.setItem('theme', state.activeTheme)
+    }
+  })
+
+  const context: ThemeContext = [
+    state,
+    {
+      toggleTheme: () =>
+        setState('activeTheme', activeTheme => (activeTheme === 'light' ? 'dark' : 'light')),
+    },
+  ]
+
+  return <ThemeContext.Provider value={context}>{props.children}</ThemeContext.Provider>
+}
+
+export const useThemeStore = (): ThemeContext => {
+  const themeContext = useContext(ThemeContext)
+
+  if (!themeContext) {
+    throw new Error('Need to pass a value to the context')
+  }
+
+  return themeContext
+}
