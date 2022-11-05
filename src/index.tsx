78d2bb7d62427272f55c1b55edbe6e1904c929cd
hey3 <oh1kohei@gmail.com>
2022-11-05 19:18:02 +0900

    feat: remove tailwindcss configures

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { render } from 'solid-js/web'
 import { App } from './app'
-import './index.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
