2cb190974ff30b48c569bea81c8530adb7758a20
hey3 <oh1kohei@gmail.com>
2022-11-05 23:10:44 +0900

    refactor: replace to use css modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import { render } from 'solid-js/web'
 import { App } from './app'
+import { ThemeProvider } from './stores/ThemeStore'
 import './styles/reset.css'
 import './styles/global.css'
 
-const rootElement = document.getElementById('root')
+const rootElement = window.document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
 
-render(() => <App />, rootElement)
+render(
+  () => (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  ),
+  rootElement
+)
