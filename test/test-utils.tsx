c906b7b512f20808b7955425338a1c39d0ff1c70
hey3 <oh1kohei@gmail.com>
2023-08-14 11:31:08 +0900

    feat: test setup and utility

---
+++ test/test-utils.tsx
@@ -0,0 +1,24 @@
+import { render } from '@solidjs/testing-library'
+import type { Component, JSX } from 'solid-js'
+import { ThemeProvider } from '../src/stores/ThemeStore'
+
+type Options = NonNullable<Parameters<typeof render>[1]>
+type Result = ReturnType<typeof render>
+
+type Props = {
+  children: JSX.Element
+}
+
+const AllTheProviders: Component<Props> = props => {
+  return <ThemeProvider>{props.children}</ThemeProvider>
+}
+
+const customRender = (ui: () => JSX.Element, options?: Options): Result =>
+  render(ui, {
+    wrapper: AllTheProviders,
+    ...options,
+  })
+
+export * from '@solidjs/testing-library'
+export { default as userEvent } from '@testing-library/user-event'
+export { customRender as render }
