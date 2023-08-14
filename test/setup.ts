6c7346d84392d76039865940cc9f9db704bf1ccc
hey3 <oh1kohei@gmail.com>
2023-08-14 11:54:24 +0900

    feat: test setup and utility

---
+++ test/setup.ts
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+
+// Mock for theme.
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+})
+
+// Reset theme for each test.
+beforeEach(() => {
+  window.localStorage.clear()
+  window.document.documentElement.setAttribute('data-theme', 'light')
+})
